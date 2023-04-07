9d57944018e4e4824c7cf56d4f931b4827f07139
Marco <patrickstar1281@yahoo.com>
2023-04-07 09:53:38 -0500

    added some style to nav bar

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 window.addEventListener('load', function() {
-    const canvas = document.getElementById('canvas1');
-    const ctx = canvas.getContext('2d');
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-
+    // navbar stuff
     const menu = document.querySelector('#mobileMenu');
     const menuLinks = document.querySelector('.navMenu');
 
@@ -11,4 +7,13 @@ window.addEventListener('load', function() {
         menu.classList.toggle('isActive');
         menuLinks.classList.toggle('active');
     });
+
+    // Welcome text stuff
+    const canvas = document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    
+    
 });
