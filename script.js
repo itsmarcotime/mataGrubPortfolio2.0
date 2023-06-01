192f3973d674ab9002ecdadfe73838526856ada8
Marco <patrickstar1281@yahoo.com>
2023-06-01 09:14:12 -0500

    added more work on work page

--- script.js
+++ script.js
@@ -16,13 +16,14 @@ for (let i = 0; i < mataGrub.length; i++) {
 
 //work page stuff 
 const buttons = [document.querySelector('#btn'), document.querySelector('#btn0'), document.querySelector('#btn1'),
-document.querySelector('#btn2')];
+document.querySelector('#btn2'), document.querySelector('#btn3'), document.querySelector('#btn4')];
 
 const panels = [document.querySelector('.panel'), document.querySelector('.panel0'), document.querySelector('.panel1'),
-document.querySelector('.panel2')];
+document.querySelector('.panel2'), document.querySelector('.panel3'), document.querySelector('.panel4')];
 
 const closeBtns = [document.querySelector('#closeBtn'), document.querySelector('#closeBtn0'), 
-document.querySelector('#closeBtn1'), document.querySelector('#closeBtn2')];
+document.querySelector('#closeBtn1'), document.querySelector('#closeBtn2'), document.querySelector('#closeBtn3'),
+document.querySelector('#closeBtn4')];
 
 buttons.forEach((button, i) => {
     button.addEventListener('click', () => {
