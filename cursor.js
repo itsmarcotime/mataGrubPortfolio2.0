afa9dd5ff7558cafb8f43922b07457c4888131bc
Marco <patrickstar1281@yahoo.com>
2023-06-07 21:21:52 -0500

    got cursor scipts started

--- cursor.js
+++ cursor.js
@@ -1,5 +1,29 @@
 const cursor = document.querySelector(".cursor");
+const links = document.querySelectorAll("a");
+let isMoving = false;
 
 document.addEventListener("mousemove", (e) => {
-    console.log(e.pageX);
+    if (!isMoving) {
+        isMoving = true;
+
+        setTimeout(() => {
+          let leftPosition = e.clientX + 4;
+          let topPosition = e.clientY + 4;
+
+          cursor.style.left = leftPosition + "px";
+          cursor.style.top = topPosition + "px";
+          isMoving = false;
+        }, 30);
+    }
+});
+
+links.forEach(link => {
+    link.addEventListener("mouseenter", () => {
+        cursor.classList.add("large");
+    });
+});
+links.forEach(link => {
+    link.addEventListener("mouseleave", () => {
+        cursor.classList.remove("large");
+    });
 });
